32201ff2dfac81caf078544ff00c676411361049
EmilianoTL <atorresl1900@alumno.ipn.mx>
2026-05-13 11:05:20 -0600

    correct sintaxixs change Cmd to _cmd

--- app/services/snmp_service.py
+++ app/services/snmp_service.py
@@ -3,7 +3,7 @@ from app.config import Config
 
 def consulta_snmp_v3(oid):
     """Realiza una consulta GET usando SNMPv3."""
-    iterator = getcmd(
+    iterator = get_cmd(
         SnmpEngine(),
         UsmUserData(
             Config.SNMP_USER, 
