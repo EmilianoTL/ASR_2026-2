06ae0af999479b183f6c70435a83bf47f2112716
EmilianoTL <atorresl1900@alumno.ipn.mx>
2026-06-04 09:27:07 -0600

    Change port 5000

--- 07_ACLsEstados/app.py
+++ 07_ACLsEstados/app.py
@@ -71,4 +71,4 @@ def delete_escenario(n: int):
 # ── Inicio ───────────────────────────────────────────────
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
